f290209eb5b1ea3c792fc25fd4e42b16a86b6454
riku-0604 <138437629+riku-0604@users.noreply.github.com>
2023-09-13 18:22:28 +0900

    double_R

--- section/DoubleLoop.h
+++ section/DoubleLoop.h
@@ -32,59 +32,59 @@ class DoubleLoop : public SectionManager {
             */
            
         SecParam spara [100] = {
-            {Section::TRACER,{50, -0.2,  33, 12, 9,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{5},Section::NONE,{}},
+            {Section::TRACER,{50, -0.2,  33, 12, 9,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{5},Section::NONE,{}},
             //{Section::TRACER,{50, -0.2,  30, 12, 9,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{2},Section::NONE,{}},
-            {Section::TRACER,{40, -0.2,  28, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{5},Section::NONE,{}},
-            {Section::TRACER,{21, -0.2,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{6},Section::NONE,{}},
+            {Section::TRACER,{40, -0.2,  28, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{5},Section::NONE,{}},
+            {Section::TRACER,{21, -0.2,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{6},Section::NONE,{}},
 
-            {Section::TRACER,{21, -0.2,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},//青マーカー検知用
+            {Section::TRACER,{21, -0.2,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},//青マーカー検知用
             //{Section::TRACER,{22, 0,  30, 10, 5,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{10},Section::NONE,{}},
             //{Section::TRACER,{22, -0.5,  30, 10, 5,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{10},Section::NONE,{}},
             {Section::WALKER,{0, 0},Section::TIME,{100},Section::NONE,{}},
             //{Section::TURNWALKER,{-50,20,5,2},Section::TURNANGLE,{1},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{100},Section::NONE,{}},
-            {Section::VIRTUALSTRAIGHT,{-10,25,20,5,3,LineTracer::LEFTEDGE},Section::LENGTH,{16},Section::NONE,{}},
+            {Section::VIRTUALSTRAIGHT,{10,25,20,5,3,LineTracer::RIGHTEDGE},Section::LENGTH,{16},Section::NONE,{}},
             
 
             //{Section::TRACER,{22, -0.5,  30, 10, 5,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{10},Section::NONE,{}},
-            {Section::TRACER,{33, 0,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{128},Section::NONE,{}},
+            {Section::TRACER,{33, 0,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{128},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{100},Section::NONE,{}},
-            {Section::TRACER,{21, -0.2,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},//青マーカー検知用
+            {Section::TRACER,{21, -0.2,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},//青マーカー検知用
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::TRACER,{23, 0,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{14},Section::NONE,{}},
-            {Section::VIRTUALSTRAIGHT,{-15,25,20,5,2,LineTracer::LEFTEDGE},Section::LENGTH,{13},Section::NONE,{}},
-            {Section::TRACER,{23, 0,  17, 12, 10,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{10},Section::NONE,{}},
-            {Section::TRACER,{30, 0,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{128},Section::NONE,{}},
+            {Section::TRACER,{23, 0,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{14},Section::NONE,{}},
+            {Section::VIRTUALSTRAIGHT,{15,25,20,5,2,LineTracer::RIGHTEDGE},Section::LENGTH,{13},Section::NONE,{}},
+            {Section::TRACER,{23, 0,  18, 12, 10,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{10},Section::NONE,{}},
+            {Section::TRACER,{30, 0,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{128},Section::NONE,{}},
             
-            {Section::TRACER,{21, -0.2,  17, 12, 7,0,0,LineTracer::LEFTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},
+            {Section::TRACER,{21, -0.2,  18, 12, 7,0,0,LineTracer::RIGHTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
             //{Section::TRACER,{0, 0, 0, 0, 0,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{500},Section::NONE,{}},//forced termination
             //{Section::WALKER,{0, 0},Section::TIME,{100},Section::NONE,{}},
-            {Section::TRACER,{21, 0,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{28.5},Section::NONE,{}},
+            {Section::TRACER,{21, 0,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{28.5},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::VIRTUALSTRAIGHT,{-9,21,20,5,2,LineTracer::LEFTEDGE},Section::LENGTH,{6},Section::NONE,{}},
-            {Section::TRACER,{31, 0,  16, 0, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{230},Section::NONE,{}},
-            {Section::TRACER,{21, -0.2, 17, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},
+            {Section::VIRTUALSTRAIGHT,{9,21,20,5,2,LineTracer::RIGHTEDGE},Section::LENGTH,{6},Section::NONE,{}},
+            {Section::TRACER,{31, 0,  16, 0, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{230},Section::NONE,{}},
+            {Section::TRACER,{21, -0.2, 18, 12, 8,0,0,LineTracer::LEFTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::TRACER,{21, -0.1,  17, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{34},Section::NONE,{}},
+            {Section::TRACER,{21, -0.1,  18, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{34},Section::NONE,{}},
             //{Section::TRACER,{30, -0.4,  40, 10, 9,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{5},Section::NONE,{}},
             
-            {Section::VIRTUALSTRAIGHT,{2,23,20,5,2,LineTracer::LEFTEDGE},Section::LENGTH,{8},Section::NONE,{}},
+            {Section::VIRTUALSTRAIGHT,{-2,23,20,5,2,LineTracer::RIGHTEDGE},Section::LENGTH,{8},Section::NONE,{}},
             //{Section::TRACER,{25, 0,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{10},Section::NONE,{}},
-            {Section::TRACER,{30, 0,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{97},Section::NONE,{}},
+            {Section::TRACER,{30, 0,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{97},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::VIRTUALSTRAIGHT,{-6,25,20,5,2,LineTracer::LEFTEDGE},Section::LENGTH,{19},Section::NONE,{}},
-            {Section::TRACER,{32, 0,  17, 10, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{43},Section::NONE,{}},
+            {Section::VIRTUALSTRAIGHT,{6,25,20,5,2,LineTracer::RIGHTEDGE},Section::LENGTH,{19},Section::NONE,{}},
+            {Section::TRACER,{32, 0,  18, 10, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{43},Section::NONE,{}},
             //{Section::TRACER,{0, 0, 0, 0, 0,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{500},Section::NONE,{}},
-            {Section::TRACER,{40, 0, 40, 12, 10,20,0,LineTracer::LEFTEDGE},Section::LENGTH,{34},Section::NONE,{}},
+            {Section::TRACER,{40, 0, 38, 13, 10,-20,0,LineTracer::RIGHTEDGE},Section::LENGTH,{34},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{100},Section::NONE,{}},
-            {Section::TRACER,{33, -0.1,  20, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{20},Section::NONE,{}},
-            {Section::TRACER,{23, -0.2,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},
-            {Section::TRACER,{70, 0,  49, 10, 12,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{73},Section::NONE,{}},
-            {Section::TRACER,{32, 0,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{30},Section::NONE,{}},
-            {Section::TRACER,{23, -0.2,  17, 12, 8,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{25},Section::NONE,{}},
+            {Section::TRACER,{33, -0.1,  20, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{20},Section::NONE,{}},
+            {Section::TRACER,{23, -0.2,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::COLOR,{228,0.16},Section::NONE,{}},
+            {Section::TRACER,{70, 0,  49, 10, 12,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{83},Section::NONE,{}},
+            {Section::TRACER,{32, 0,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{30},Section::NONE,{}},
+            {Section::TRACER,{23, -0.2,  18, 12, 8,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{25},Section::NONE,{}},
             {Section::WALKER,{0, 0},Section::TIME,{80},Section::NONE,{}},
-            {Section::VIRTUALSTRAIGHT,{0,17,20,5,2,LineTracer::LEFTEDGE},Section::COLOR,{20,0.5},Section::NONE,{}},//赤
+            {Section::VIRTUALSTRAIGHT,{0,18,20,5,2,LineTracer::RIGHTEDGE},Section::COLOR,{20,0.5},Section::NONE,{}},//赤
             //{Section::TRACER,{25, 0,  30, 10, 7,0,0,LineTracer::LEFTEDGE},Section::COLOR,{15,0.1},Section::NONE,{}},
             
             {Section::TRACER,{0, 0, 0, 0, 0,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{500},Section::NONE,{}},
--- section/SpeedSectionManager.h
+++ section/SpeedSectionManager.h
@@ -10,42 +10,21 @@ class SpeedSectionManager : public SectionManager {
     protected:
 
     private:
-<<<<<<< Updated upstream
-=======
-        /*?��?��?��?��
-            LENGTH,{?��?��?��?��}
-            TIME,{?��?��?��?��}
-            COLOR,{?��F,?��ʓx}
-            BLOCKCOLOR,{?��ԃu?��?��?��b?��N,?��u?��?��?��b?��N,?��ʓx,?��?��?��?��}
-            TURNANGLE,{?��?��?��?��p?��x}  //?��?��?��s?��̂�?��?���?��l?��͉E?��?��]?��?��?��}?��C?��i?��X?��A?��?��?��?��]?��̓v?��?��?��X//
-            ARMANGLE,{?��A?��[?��?��?��p?��x}
-            TEELANGLE,{?��?��?��?��?��ۂ̖ڕW?��p?��x}
-            ENDJ,
-            NONE
-            */
-
-           /*?��?��?��?��
-            WALKER,{?��O?��i?��̃p?��?��?��[,?��?��?��?��̃p?��?��?��[}  //?��?��?��?��̃p?��?��?��[?��͉E?��?��]?��?��?��v?��?��?��X?��A?��?��?��?��]?��̓}?��C?��i?��X//
-            TRACER,{?��?��?��x,?��?��?��?��,P,I,D,?��p?��x?��ڕW?��l,?��?��?��?��?��P}
-            ARM,{?��ڕW?��̃A?��[?��?��?��̊p?��x}
-            TEEL,{?��?��?��?��?��ۂ�??��?��[?��^?��[?��̋�?��?��}
-            VIRTUALSTRAIGHT,{?��?��?��s?��p?��x,?��?��?��x,P,I,D}
-            END
-            */
+       
            
         SecParam spara [100] = {
             {Section::WALKER,{0, 0},Section::TIME,{1},Section::NONE,{}},
             {Section::DEFOLTARMCHANGE,{-15},Section::TIME,{3},Section::NONE,{}},
             //{Section::TRACER,{50, 0,  48, 10, 13,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{13},Section::NONE,{}},
-            {Section::TRACER,{80, 0,  49, 10, 13,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{296},Section::NONE,{}},
+            {Section::TRACER,{66, 0,  49, 10, 13,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{296},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::TRACER,{50, 0,  40, 12, 10,20,0,LineTracer::LEFTEDGE},Section::LENGTH,{36},Section::NONE,{}},
+            {Section::TRACER,{40, 0,  40, 12, 10,-20,0,LineTracer::RIGHTEDGE},Section::LENGTH,{36},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::TRACER,{80, 0,  49, 10, 13,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{175},Section::NONE,{}},
+            {Section::TRACER,{66, 0,  49, 10, 13,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{175},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::TRACER,{50, 0,  40, 12, 10,20,0,LineTracer::LEFTEDGE},Section::LENGTH,{36},Section::NONE,{}},
+            {Section::TRACER,{40, 0,  40, 12, 10,-20,0,LineTracer::RIGHTEDGE},Section::LENGTH,{36},Section::NONE,{}},
             //{Section::WALKER,{0, 0},Section::TIME,{150},Section::NONE,{}},
-            {Section::TRACER,{60, 0,  49, 10, 13,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{13},Section::NONE,{}},//LAP
+            {Section::TRACER,{65, 0,  49, 10, 13,0,0,LineTracer::RIGHTEDGE},Section::LENGTH,{13},Section::NONE,{}},//LAP
             
             //{Section::TRACER,{0, 0, 0, 0, 0,0,0,LineTracer::LEFTEDGE},Section::LENGTH,{500},Section::NONE,{}},
             {Section::END,{},Section::ENDJ,{}},
@@ -53,7 +32,6 @@ class SpeedSectionManager : public SectionManager {
         /*{
             {LineTracer,},{},{},{},{}}
             */
->>>>>>> Stashed changes
 
 };
 
